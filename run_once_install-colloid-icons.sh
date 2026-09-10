4dd00b238f47cc6f1a1f57f7cbc45bbb002ec9e5
AksharLeo <aksharthirunahari@gmail.com>
2026-09-10 20:36:46 +0530

    Update dotfiles

--- run_once_install-colloid-icons.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-tmpdir="$(mktemp -d)"
-trap 'rm -rf "$tmpdir"' EXIT
-
-git clone --depth=1 https://github.com/vinceliuice/Colloid-icon-theme "$tmpdir/Colloid-icon-theme"
-"$tmpdir/Colloid-icon-theme/install.sh" -t red -n Colloid-Pastel
