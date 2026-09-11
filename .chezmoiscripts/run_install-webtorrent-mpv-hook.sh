9300ae6b69ba5114402cad6a521b8e67fcca90a4
AksharLeo <aksharthirunahari@gmail.com>
2026-09-12 00:10:29 +0530

    update mpv-hook script

--- .chezmoiscripts/run_install-webtorrent-mpv-hook.sh
+++ .chezmoiscripts/run_install-webtorrent-mpv-hook.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 set -eu
 
+export NVM_DIR="${NVM_DIR:-$HOME/.nvm}"
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  . "$NVM_DIR/nvm.sh"
+fi
+
+command -v npm >/dev/null 2>&1 || {
+  echo "npm not found" >&2
+  exit 1
+}
+
 npm install --global webtorrent-mpv-hook@latest
 
-PACKAGE="$(npm root --global)/webtorrent-mpv-hook"
+package="$(npm root --global)/webtorrent-mpv-hook"
 
 mkdir -p "$HOME/.config/mpv/scripts"
 
 ln -sfn \
-  "$PACKAGE/build/webtorrent.js" \
+  "$package/build/webtorrent.js" \
   "$HOME/.config/mpv/scripts/webtorrent.js"
